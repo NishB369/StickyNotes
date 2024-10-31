e86ee52d782517564670a94f3b5be7df4ba8423e
nishb369 <nishbcodes@gmail.com>
2024-10-31 23:42:16 +0530

    js added

---
+++ script.js
@@ -0,0 +1,26 @@
+const notesContainer = document.querySelector("#app")
+const addNoteButton = document.querySelector(".add-note")
+
+function getNotes(){
+    
+}
+
+function saveNotes(){
+
+}
+
+function createNoteElement(id, content){
+
+}
+
+function addNote(){
+
+}
+
+function updateNote(){
+
+}
+
+function deleteNote(){
+
+}
